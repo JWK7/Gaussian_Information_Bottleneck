5d1ffc272359b0ca8efe8e372ca9528f51018241
jwk7 <johnwooyoungkim@gmail.com>
2025-01-31 21:57:00 -0600

    Update

--- GIB.py
+++ GIB.py
@@ -4,8 +4,8 @@ import numpy as np
 def get_epsilon(data_size , cov):
     return np.random.normal(0,cov,data_size)
 
-def direct_A(kwargs):
-    return np.matmul(kwargs["cov_XgY"], np.linalg.inv(kwargs["cov_X"]))
+# def direct_A(**kwargs):
+#     return np.linalg.eig(np.matmul(kwargs["cov_XgY"], np.linalg.inv(kwargs["cov_X"])))
 
 
 def get_covs(A,B):
@@ -16,11 +16,26 @@ def get_covs(A,B):
     cov_AgB = cov_AA - np.matmul( np.matmul(cov_AB , np.linalg.inv(cov_BB)) , cov_AB.T )
     return cov_AA,cov_AB,cov_BB,cov_AgB
 
-def get_init_A(full_size,compressed_size):
-    return np.random.uniform(-1,1,(compressed_size,full_size))
+def get_alpha(**kwargs):
+    r = get_r(**kwargs)
+    return ( ( kwargs["Beta"]*(1-kwargs["lamda"])-1)  / (r*kwargs["lamda"]) )
+
+def get_critical_points(lamda): return 1/(1-lamda)
+
+def get_init_A(**kwargs):
+    #V is full size A while D is the diagonal matrix of lambda
+    cov_X,_,_,cov_XgY = get_covs(kwargs["X"],kwargs["Y"])
+    lamda,V= np.linalg.eig(np.matmul(cov_XgY, np.linalg.inv(cov_X)))
+    alpha = get_alpha(lamda=lamda,cov_X=cov_X,V=V,Beta = kwargs["Beta"])
+    B_c = get_critical_points(lamda)
+    full_A = alpha*V
+    return ( alpha * (kwargs["Beta"] < B_c) * full_A )
 
 def get_init_Cov_Epsil(A_shape):
-    return np.cov(np.random.normal(-0.1,0.1,A_shape))
+    return np.cov(np.random.normal(-0.01,0.01,A_shape))
+
+def get_r(**kwargs):
+    return np.diagonal( np.matmul( np.matmul(kwargs["V"].T,kwargs["cov_X"]) , kwargs["V"]) )
 
 def get_T(A,X,cov_epsil):
     # print(np.random.multivariate_normal([0,0,0,0,0],cov_epsil).shape)
@@ -36,17 +51,22 @@ def get_p(X,Y):
     return I - np.matmul(covYgX,covX)
 
 def GIB(X,Y):
-    Beta = 1
-    A = get_init_A(X.shape[0],X.shape[0])
+    Beta = 1.005
+    A = get_init_A(X=X,Y=Y,Beta=Beta)
+    print(A)
     cov_epsil = get_init_Cov_Epsil(A.shape)
+
     #p = I - cov(Y|X) inv(cov(X)) 
     p = get_p(X,Y)
+    if (np.allclose(A, 0)): raise SystemExit('B < B_c1')
 
-    for _ in range(100):
+    for i in range(100):
         T = get_T(A,X,cov_epsil)
         cov_T, _,_,cov_TgY = get_covs(T,Y)
         cov_epsil = np.linalg.inv(Beta * np.linalg.inv(cov_TgY) - (Beta-1) * np.linalg.inv(cov_T))
         A = Beta * np.matmul( np.matmul( np.matmul(cov_epsil,np.linalg.inv(cov_TgY)) , A) , p)
+        # print(i)
+    print(A)
         
 
 def main():
--- generate_joint_gaussian_random_variables.py
+++ generate_joint_gaussian_random_variables.py
@@ -2,10 +2,10 @@ from numpy.random import normal,uniform
 def generate_joint_gaussian_random_variables(sample_size,n_var):
     u = normal(0,1,(n_var,sample_size))
     v = normal(0,1,(n_var,sample_size))
-    a = uniform(10,(n_var))
-    b = uniform(10,(n_var))
-    c = uniform(10,(n_var))
-    d = uniform(10,(n_var))
+    a = uniform(2,(n_var))
+    b = uniform(2,(n_var))
+    c = uniform(2,(n_var))
+    d = uniform(2,(n_var))
     X = a * u + b * v
     Y = c * u + d * v
     return X,Y
