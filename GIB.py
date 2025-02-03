7302cbec72bb944aed1a1ce7f40cd3f8cc544561
jwk7 <johnwooyoungkim@gmail.com>
2025-02-03 14:26:40 -0600

    GIB Information Curve-Note need to clean code

--- GIB.py
+++ GIB.py
@@ -1,19 +1,17 @@
 from generate_joint_gaussian_random_variables import generate_joint_gaussian_random_variables
 import numpy as np
+from scipy.linalg import eig
+import matplotlib.pyplot as plt
 
 def get_epsilon(data_size , cov):
-    return np.random.normal(0,cov,data_size)
-
-# def direct_A(**kwargs):
-#     return np.linalg.eig(np.matmul(kwargs["cov_XgY"], np.linalg.inv(kwargs["cov_X"])))
+    return np.random.multivariate_normal([0]*cov.shape[0],cov,data_size)
 
 
-def get_covs(A,B):
-    cov = np.cov(A,B)
+def get_covs(cov):
     cov_AA = cov[:cov.shape[0]//2,:cov.shape[0]//2]
     cov_AB = cov[:cov.shape[0]//2,cov.shape[0]//2:]
     cov_BB = cov[cov.shape[0]//2:,cov.shape[0]//2:]
-    cov_AgB = cov_AA - np.matmul( np.matmul(cov_AB , np.linalg.inv(cov_BB)) , cov_AB.T )
+    cov_AgB = cov_AA - cov_AB  @  np.linalg.inv(cov_BB) @ cov_AB.T
     return cov_AA,cov_AB,cov_BB,cov_AgB
 
 def get_alpha(**kwargs):
@@ -24,54 +22,154 @@ def get_critical_points(lamda): return 1/(1-lamda)
 
 def get_init_A(**kwargs):
     #V is full size A while D is the diagonal matrix of lambda
-    cov_X,_,_,cov_XgY = get_covs(kwargs["X"],kwargs["Y"])
-    lamda,V= np.linalg.eig(np.matmul(cov_XgY, np.linalg.inv(cov_X)))
+    cov_X,_,_,cov_XgY = get_covs(kwargs["Sigma"])
+    lamda ,_ = np.linalg.eig(cov_XgY @ np.linalg.inv(cov_X))
+    _, V = np.linalg.eig((cov_XgY @ np.linalg.inv(cov_X)).T)
+    V = (np.linalg.inv(V))
+    # lamda,V,_ = eig(cov_XgY @ np.linalg.inv(cov_X),left=True)
+
+    sort_index = np.argsort(lamda)
+    lamda = lamda[sort_index]
+    V = V[:,sort_index]
+
     alpha = get_alpha(lamda=lamda,cov_X=cov_X,V=V,Beta = kwargs["Beta"])
     B_c = get_critical_points(lamda)
-    full_A = alpha*V
-    return ( alpha * (kwargs["Beta"] < B_c) * full_A )
+    if all(x == False for x in (kwargs["Beta"] > B_c)): print('B < B_c1: Full Noise')
+    if all(x == True for x in (kwargs["Beta"] > B_c)): print('B > B_cn: No Compression')
+    # if all(x == False for x in (kwargs["Beta"] > B_c)): raise SystemExit('B < B_c1: Full Noise')
+    # if all(x == True for x in (kwargs["Beta"] > B_c)): raise SystemExit('B > B_cn: No Compression')
+    full_A = np.expand_dims(alpha,-1)*V
+    return np.expand_dims( (kwargs["Beta"] > B_c),-1)* full_A
 
 def get_init_Cov_Epsil(A_shape):
-    return np.cov(np.random.normal(-0.01,0.01,A_shape))
+    return np.identity(A_shape[0])*0.3
+    E = np.random.uniform( 0,1, A_shape)
+    # _,E = np.linalg.eig(E @ E.T)
+    return E@E.T
 
 def get_r(**kwargs):
-    return np.diagonal( np.matmul( np.matmul(kwargs["V"].T,kwargs["cov_X"]) , kwargs["V"]) )
+    return np.diagonal( kwargs["V"].T@kwargs["cov_X"] @kwargs["V"])
 
 def get_T(A,X,cov_epsil):
-    # print(np.random.multivariate_normal([0,0,0,0,0],cov_epsil).shape)
-    # print(np.matmul(A,X).shape)[0,]*A.shape[0]
-    AX = np.matmul(A,X)
-    epsil = np.random.multivariate_normal([0,]*A.shape[0],cov_epsil)
-    return np.add(epsil.reshape(*epsil.shape,1),AX)
+    AX = A@X
+    epsil = np.random.multivariate_normal([0,]*A.shape[0],cov_epsil,X.shape[1]).T
+    # for i in range(X.shape[1]):
+        # X[:,i] = np.random.multivariate_normal([0,]*A.shape[0],cov_epsil)
+    return epsil + AX
 
-def get_p(X,Y):
-    I = np.identity(X.shape[0])
-    _,_,_,covYgX = get_covs(Y,X)
-    covX = np.cov(X)
-    return I - np.matmul(covYgX,covX)
+def get_p(Sigma):
+    _,_,covX,covYgX = get_covs(Sigma[::-1,::-1])
+    I = np.identity(covX.shape[0])
+    return I - covYgX @ np.linalg.inv(covX)
 
-def GIB(X,Y):
-    Beta = 1.005
-    A = get_init_A(X=X,Y=Y,Beta=Beta)
-    print(A)
+def GIB(B,Sigma):
+    A = get_init_A(Beta=B,Sigma=Sigma)
     cov_epsil = get_init_Cov_Epsil(A.shape)
+    # cov_epsil = np.identity(A.shape[0])
 
     #p = I - cov(Y|X) inv(cov(X)) 
-    p = get_p(X,Y)
-    if (np.allclose(A, 0)): raise SystemExit('B < B_c1')
+    p = get_p(Sigma)
+    cov_X,cov_XY,cov_Y,cov_XgY = get_covs(Sigma)
 
-    for i in range(100):
-        T = get_T(A,X,cov_epsil)
-        cov_T, _,_,cov_TgY = get_covs(T,Y)
+    cov_T = A@cov_X@A.T + cov_epsil
+    cov_TX = A@cov_X 
+    cov_TY = A@cov_XY
+    
+    IXT = get_mutual_information(cov_X,cov_TX.T,cov_T)
+    ITY = get_mutual_information(cov_T,cov_TY,cov_Y)
+    return IXT,ITY
+
+def get_entropy(d,Sigma):
+    # print(np.log(np.linalg.det(Sigma)))
+    return (d/2)*np.log(1+2*np.pi) + (1/2)*np.log(np.linalg.det(Sigma))
+
+def get_mutual_information(cov_A,cov_AB,cov_B):
+    Sigma = np.zeros( ( 2*cov_A.shape[0] , 2*cov_A.shape[0] ) )
+    Sigma[:Sigma.shape[0]//2,:Sigma.shape[0]//2] = cov_A
+    Sigma[:Sigma.shape[0]//2,Sigma.shape[0]//2:] = cov_AB
+    Sigma[Sigma.shape[0]//2:,:Sigma.shape[0]//2] = cov_AB.T
+    Sigma[Sigma.shape[0]//2:,Sigma.shape[0]//2:] = cov_B
+    # print(np.linalg.det(Sigma))
+    # print(cov_A.shape)
+    # print(np.linalg.det(Sigma))
+    H_A = get_entropy(cov_A.shape[0],cov_A)
+    H_B = ( get_entropy(cov_B.shape[0],cov_B))
+    H = ( get_entropy(Sigma.shape[0],Sigma))
+    return (1/2)*np.log(H_A*H_B/H)
+
+def Iterative_GIB(X,Y,Sigma):
+
+    Beta = 1.1
+    A = get_init_A(Beta=Beta,Sigma=Sigma)
+    cov_epsil = get_init_Cov_Epsil(A.shape)
+    # cov_epsil = np.identity(A.shape[0])
+
+    #p = I - cov(Y|X) inv(cov(X)) 
+    p = get_p(Sigma)
+    cov_x,_,_,cov_xgy = get_covs(Sigma)
+
+    # print(A) 
+    # exit()
+
+
+    for i in range(10000):
+        # T = get_T(A,X,cov_epsil)
+        # cov_T, _,_,cov_TgY = get_covs( np.cov(T,Y) )
+        # print(cov_T)
+        cov_T = A@cov_x@A.T+ cov_epsil
+        # print(cov_xgy)
+        # print(A)
+        # print(A@ cov_xgy @A.T+ cov_epsil)
+        # exit()
+        cov_TgY = A@ cov_xgy @A.T + cov_epsil
+        # input()
+        # exit()
+        # print(cov_epsil)
+        # print(np.cov(T,Y))
+        # print()
+        # print(cov_T)
+        # print(cov_epsil)
+        # print(cov_TgY)
+        # exit()
+        print(cov_epsil)
         cov_epsil = np.linalg.inv(Beta * np.linalg.inv(cov_TgY) - (Beta-1) * np.linalg.inv(cov_T))
-        A = Beta * np.matmul( np.matmul( np.matmul(cov_epsil,np.linalg.inv(cov_TgY)) , A) , p)
-        # print(i)
+        A_prev = A
+        A = Beta * cov_epsil @ np.linalg.inv(cov_TgY) @ A @  p
+        # print(A)
+        print( (A-A_prev)[0][0] )
+
+        # if i == 0:
+        #     exit()
+        # print(A)
+        # print(cov_epsil)
+        # exit()
+        # print(A)
     print(A)
-        
+def PlotOverB():
+    _,_,Sigma = generate_joint_gaussian_random_variables(10000,5)
+    cov_X,cov_XY,covY,covXgY = get_covs(Sigma)
+    eigenval,_ = np.linalg.eig(covXgY@np.linalg.inv(cov_X))
+    eigenval = (np.sort(eigenval))
+    Bs = 1/(1-eigenval)
+    Bs = np.append( Bs, (Bs[-1]+1) )
+    # print(range(1000))
+    # exit()
+    IXTs = []
+    ITYs = []
+    # for B in Bs:
+    for B in range(6000):
+        # print(B/10)
+        IXT, ITY = GIB(B/10,Sigma)
+        IXTs.append(IXT)
+        ITYs.append(ITY)
+    plt.plot(IXTs,ITYs)
+    plt.savefig("GIB_Information_Curve.png")
+    
 
 def main():
-    X,Y = generate_joint_gaussian_random_variables(20,5)
-    GIB(X,Y)
+    PlotOverB()
+    # X,Y,Sigma = generate_joint_gaussian_random_variables(100000,5)
+    # Iterative_GIB(X,Y,Sigma)
 
 if __name__ == "__main__":
     main()
--- generate_joint_gaussian_random_variables.py
+++ generate_joint_gaussian_random_variables.py
@@ -1,15 +1,63 @@
-from numpy.random import normal,uniform
+from numpy.random import normal,uniform, multivariate_normal
+import numpy as np
 def generate_joint_gaussian_random_variables(sample_size,n_var):
-    u = normal(0,1,(n_var,sample_size))
-    v = normal(0,1,(n_var,sample_size))
-    a = uniform(2,(n_var))
-    b = uniform(2,(n_var))
-    c = uniform(2,(n_var))
-    d = uniform(2,(n_var))
-    X = a * u + b * v
-    Y = c * u + d * v
-    return X,Y
+    # u = normal(0,1,(n_var,sample_size))
+    # v = normal(0,1,(n_var,sample_size))
+    # a = uniform(2,(n_var))
+    # b = uniform(2,(n_var))
+    # c = uniform(2,(n_var))
+    # d = uniform(2,(n_var))
+    # X = a * u + b * v
+    # Y = c * u + d * v
+    # mean = [0]*n_var
+    # cov = [[1, 0.5], [0.5, 1]]
+
+    # A = np.random.rand(n_var, n_var)
+    # B = np.dot(A, A.transpose())
+
+    # samples = multivariate_normal(mean=mean, cov=B,size =(sample_size,5))
+    # print(samples)
+    # # exit()
+    # return samples[:,0],samples[:,1]
+
+    # Define dimensions
+    d = 5
+    m = 5
+
+    mu_X = np.array([0, 0, 0, 0, 0])
+    mu_Y = np.array([0, 0, 0, 0, 0])
+
+    mu = np.concatenate([mu_X, mu_Y])
+
+    # Sigma_XX = np.array([[1.0, 0.5, 0.3],
+    #                     [0.5, 1.0, 0.2],
+    #                     [0.3, 0.2, 1.0]])
+
+    # Sigma_YY = np.array([[1.5, 0.7, 0.4],
+    #                     [0.7, 1.2, 0.6],
+    #                     [0.4, 0.6, 1.8]])
+
+    # Sigma_XY = np.array([[0.4, 0.2, 0.1],
+    #                     [0.3, 0.5, 0.2],
+    #                     [0.6, 0.8, 0.3]])
+
+    # Sigma_YX = Sigma_XY.T
+
+    A = np.random.uniform( 0,2, (d + m, d + m) )   # Random matrix
+    Sigma = A @ A.T
+    # print(Sigma)
+    # exit()
+    # _,Sigma = np.linalg.eig((A @ A.T))  # Ensure it's symmetric and positive semi-definite
+
+    # Generate multivariate normal samples
+    data = np.random.multivariate_normal(mu, Sigma, sample_size)
+    # Sigma = np.block([[Sigma_XX, Sigma_XY], 
+    #                 [Sigma_YX, Sigma_YY]])
+    # data = np.random.multivariate_normal(mu, Sigma, sample_size)
+    X = data[:, :d].T
+    Y = data[:, d:].T
+    return X,Y,Sigma
 
 
 if __name__ == "__main__":
-    generate_joint_gaussian_random_variables(100)
+    generate_joint_gaussian_random_variables(100,10)
